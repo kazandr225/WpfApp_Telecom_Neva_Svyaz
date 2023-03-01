53fd0e6805fa3966a424514d1dd6482e268f13bf
Данил Казанский <danii.kazanskiy@mail.ru>
2023-03-01 13:06:49 +0300

    Добавьте файлы проекта.

---
+++ WpfApp_Telecom_Neva_Svyaz/AutorizationWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp_Telecom_Neva_Svyaz
+{
+    /// <summary>
+    /// Логика взаимодействия для AutorizationWindow.xaml
+    /// </summary>
+    public partial class AutorizationWindow : Window
+    {
+        public AutorizationWindow()
+        {
+            InitializeComponent();
+
+            //Устанавливаем окно по центру экрана
+            double screenHeight = SystemParameters.FullPrimaryScreenHeight;
+            double screenWidth = SystemParameters.FullPrimaryScreenWidth;
+            this.Top = (screenHeight - this.Height) / 2;
+            this.Left = (screenWidth - this.Width) / 2;
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вывел");
+        }
+    }
+}
