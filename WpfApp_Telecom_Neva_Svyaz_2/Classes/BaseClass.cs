427f4bf78f9e04cffdda797410618c8a44ff5f1a
kazandr <danii.kazanskiy@mail.ru>
2023-03-21 00:21:15 +0300

    gh

---
+++ WpfApp_Telecom_Neva_Svyaz_2/Classes/BaseClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_Telecom_Neva_Svyaz_2.Classes
+{
+    internal class BaseClass
+    {
+        public static EntitiesModel EM;
+    }
+}
---
+++ WpfApp_Telecom_Neva_Svyaz_2/Pages/ClientsPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp_Telecom_Neva_Svyaz_2.Classes;
+
+namespace WpfApp_Telecom_Neva_Svyaz_2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsPage.xaml
+    /// </summary>
+    public partial class ClientsPage : Page
+    {
+        public ClientsPage()
+        {
+            InitializeComponent();
+            dgSubscribers.ItemsSource = BaseClass.EM.Subscriber.ToList();
+            cbActive.IsChecked = true; //вывод клиентов с активными договорами
+        }
+
+        private void cbActive_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cbNotActive_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
